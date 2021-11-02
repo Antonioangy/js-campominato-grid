8dc38065d1b6d51094d07c63383225257f26eeac
Antonioangy <dumitruangy26@gmail.com>
2021-11-02 17:57:40 +0100

    js

--- js/script.js
+++ js/script.js
@@ -9,11 +9,35 @@ const container = document.getElementById("container");
 // chiedo all'utente la difficolta
 let numeroUtente = (prompt('inserisci la difficolta con un numero da 1 a 3'));
 
-// creo un ciclo for per la difficolta' 1
-if (numeroUtente == 1){
-    for (let i = 0; i < 100; i++){
+let difficult = 100;
+let nome = null;
+if (numeroUtente == 1) {
+    difficult = 100;
+    nome = 'facile';
 }
 
+if (numeroUtente == 2) {
+    difficult = 81;
+    nome = 'medio';
+}
+
+if (numeroUtente == 3) {
+    difficult = 49;
+    nome = 'difficile';
+}
+
+// creo un ciclo for per la difficolta' 1
+
+for (let i = 0; i < difficult; i++) {
+    console.log(i);
+    let nodo = document.createElement('div');
+    nodo.addEventListener("click", function(){
+        nodo.classList.add('click');
+    })
+    nodo.classList.add('square');
+    nodo.classList.add(nome);
+    nodo.innerText = i;
+    document.querySelector('#grid').append(nodo);
 }
 
 
